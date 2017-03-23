672e73f944c0814f35cf66be477a13e7dc1d7a5c
Eri Irawan <email": null>
2017-03-23 13:35:58 +0700

    add model for article

---
+++ server/models/article.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+require('../config/db')
+
+let articleSchema = new Schema({
+  title: String,
+  content: String,
+  author: [{
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }]
+}, {
+  timestamps: true
+})
+
+let Article = mongoose.model('Article', articleSchema)
+
+module.exports = Article
