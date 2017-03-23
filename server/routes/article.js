9b0b3a82416676c506f7339aafa04bf19a8ce5bc
Eri Irawan <email": null>
2017-03-23 14:40:13 +0700

    add endpoint put and delete

--- server/routes/article.js
+++ server/routes/article.js
@@ -4,11 +4,9 @@ const express = require('express')
 const router = express.Router()
 const controller = require('../controllers/articleController')
 
-router.get('/index', function(req, res) {
-  res.send('SUCCSESS')
-})
-
 router.get('/articles', controller.getArticles)
 router.post('/article', controller.createArticle)
+router.put('/article/:slug', controller.updateArticle)
+router.delete('/article/:slug', controller.deleteArticle)
 
 module.exports = router
