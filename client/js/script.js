9eb91b5a0756eea82b91eeae4185bef515d6c04b
Eri Irawan <email": null>
2017-03-23 15:43:29 +0700

    add jQuery for getArticles, createArticle, removeArticle

--- client/js/script.js
+++ client/js/script.js
@@ -1,58 +1,62 @@
-$(document).ready(function () {
+$(document).ready(function() {
   let token = localStorage.getItem('token')
   if (!token) {
     window.location.href = 'http://127.0.0.1:8080/index.html'
   } else {
     $.ajax({
       url: `http://localhost:3000/api/verify/${token}`,
-      success: function (user) {
+      success: function(user) {
         if (!user.email) {
           window.location.href = 'http://127.0.0:8080/api/index.html'
         }
       }
     })
-    getUsers()
+    getArticles()
   }
 })
 
-$('#logout').click(function () {
+$('#logout').click(function() {
   localStorage.clear()
   window.location.href = 'http://127.0.0.1:8080/index.html'
 })
 
-function getUsers () {
+function getArticles() {
   $.ajax({
-    url: 'http://localhost:3000/api/users',
+    url: 'http://localhost:3000/api/articles',
     type: 'GET',
-    success: function (users) {
-      users.forEach(function (user) {
-        $('#user').append(`<tr><td>${user.email}</td><td><button class="waves-effect waves-light btn">edit</button>&nbsp;<button class="waves-effect waves-light btn">delete</button></td></tr>`)
+    success: function(articles) {
+      articles.article.forEach(function(article) {
+        $('#article').append(`<td>${article.title}</td><td>${article.content}</td><td>${article.author}</td><td><button class="waves-effect waves-light btn">edit</button>&nbsp;<button class="waves-effect waves-light btn">delete</button></td>`)
       })
     }
   })
 }
 
-function addTodo () {
+function createArticle() {
   $.ajax({
-    url: 'http://localhost:3000/api/user',
-    type: 'POST',
-    data: {
-      email: $('#email').val(),
-      password: $('#password').val()
+        url: 'http://localhost:3000/api/article',
+        type: 'POST',
+        data: {
+          title: $('#title').val(),
+          content: $('#content').val(),
+          author: $('#author').val()
+        },
+        success: function(user) {
+          $('#article').append(`<td>${article.title}</td><td>${article.content}</td><td>${article.author}</td><td><button class="waves-effect waves-light btn">edit</button>&nbsp;<button class="waves-effect waves-light btn">delete</button></td>`)
+        })
     },
-    success: function (user) {},
-    error: function (err) {
+    error: function(err) {
       console.log(err)
     }
-  })
+})
 }
 
-function remove (email) {
+function remove(slug) {
   $.ajax({
-    url: `http://localhost:3000/api/user/${email}`,
+    url: `http://localhost:3000/api/article/${slug}`,
     type: 'DELETE',
-    success: function (user) {
-      $(`#email`).remove()
+    success: function(user) {
+      $(`#article`).remove()
     }
   })
 }
