7a505be01c9f9e858a402ed77c01fc79d9cdae03
Eri Irawan <email": null>
2017-03-23 16:45:50 +0700

    add endpoint for get one article

--- server/routes/article.js
+++ server/routes/article.js
@@ -5,6 +5,7 @@ const router = express.Router()
 const controller = require('../controllers/articleController')
 
 router.get('/articles', controller.getArticles)
+router.get('/article/:slug', controller.getArticle)
 router.post('/article', controller.createArticle)
 router.put('/article/:slug', controller.updateArticle)
 router.delete('/article/:slug', controller.deleteArticle)
